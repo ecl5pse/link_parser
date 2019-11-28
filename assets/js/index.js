7654c8e4518580cbb04c0af492fc0304678e2794
ecl5pse <fantamas20@gmail.com>
2019-11-28 20:22:49 +0200

    modif

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,50 @@
 'use strict';
 
 const LINK_PATTERN = /<a .*?href\s*?=\s*?"(?<linkValue>.*?)".*?>(?<linkName>.*?)<\/a\s*?>/g;
+const LINK_PARSER_PATTARN = new RegExp(LINK_PATTERN.source);
+
+const textareaElem = document.querySelector("textarea[name = 'userHtml']");
+const fromElem = document.getElementsByTagName("form")[0];
+const tbodyElem = document.getElementsByTagName('tbody')[0];
+
+fromElem.onsubmit = function () {
+
+    const result = textareaElem.value.match(LINK_PATTERN);
+    tbodyElem.innerHTML = "";
+    result.forEach(
+        link =>{
+            const matchResult = link.match(LINK_PARSER_PATTARN);
+            if(matchResult){
+                const {groups : {linkValue , linkName}} = matchResult;
+                tbodyElem.appendChild(createTableRow(linkName , linkValue));
+            }
+        }
+    );
+    
+    return false;
+
+};
+
+
+
+function createTableRow(linkName, linkValue) {
+
+    const tableRowElem = document.createElement('tr');
+    const tableDataLinkNameElem = document.createElement('tb');
+    const tableDataLinkValueElem = document.createElement('td');
+
+
+    tableDataLinkNameElem.innerText = linkName;
+    tableDataLinkValueElem.innerText = linkValue;
+
+
+    tableRowElem.appendChild(tableDataLinkNameElem);
+    tableRowElem.appendChild(tableDataLinkValueElem);
+
+
+    return tableRowElem;
+    
+}
 
 
 
