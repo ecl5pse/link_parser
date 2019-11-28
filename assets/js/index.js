3d4853627427446f6deb9a7285070f18ef45e075
User Name <artur.it2914@gmail.com>
2019-11-28 19:39:27 +0200

    Create: start config

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,59 +1,7 @@
 'use strict';
 
-const PATTERN_GLOBAL = /<a.*?href\s*?=\s*?"(.*?)".*?>(.*?)<\/a\s*?>/igm;
-const PATTERN_LOCAL = new RegExp(PATTERN_GLOBAL.source, "i");
-
-const formElem = document.getElementsByTagName("form")[0];
-
-const tableBodyElem = document.getElementsByTagName("tbody")[0];
-
-formElem.onsubmit = onFormSubmit;
+const LINK_PATTERN = /<a .*?href\s*?=\s*?"(?<linkValue>.*?)".*?>(?<linkName>.*?)<\/a\s*?>/g;
 
 
-function onFormSubmit(event) {
-
-    const result = this.elements["userHtml"].value.match(PATTERN_GLOBAL);
-    tableBodyElem.innerHTML = "";
-    if (Array.isArray(result)) {
-
-        result.forEach(
-            link => {
-                 const matchResult = link.match(PATTERN_LOCAL);
-                 if(matchResult){
-                     const [,linkValue, linkName] = matchResult;
-                     tableBodyElem.appendChild(createTableRow(linkName,linkValue ))
-                 }
-
-            }
-        )
-
-
-    }
-
-
-    return false;
-
-
-}
-
-
-function createTableRow(linkName, linkValue) {
-    const trElem = document.createElement("tr");
-
-    const tdName = document.createElement("td");
-    tdName.innerText = linkName;
-
-    trElem.appendChild(
-        tdName
-    );
-
-    const tdValue = document.createElement("td");
-    tdValue.innerText = linkValue;
-    trElem.appendChild(
-        tdValue
-    );
-    return trElem;
-
-}
 
 
