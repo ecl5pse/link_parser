829e3d5322dfdfc677cad44ee9cedd6276987e18
User Name <artur.it2914@gmail.com>
2019-11-28 16:56:57 +0200

    Add: tbody creator

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,16 +10,24 @@ const tableBodyElem = document.getElementsByTagName("tbody")[0];
 formElem.onsubmit = onFormSubmit;
 
 
-
-
 function onFormSubmit(event) {
 
     const result = this.elements["userHtml"].value.match(PATTERN_GLOBAL);
+    tableBodyElem.innerHTML = "";
+    if (Array.isArray(result)) {
 
-    if(Array.isArray(result)){
+        result.forEach(
+            link => {
+                 const matchResult = link.match(PATTERN_LOCAL);
+                 if(matchResult){
+                     const [,linkValue, linkName] = matchResult;
+                     tableBodyElem.appendChild(createTableRow(linkName,linkValue ))
+                 }
 
-        const result2 = result[0].match(PATTERN_LOCAL);
-        console.log(result2);
+            }
+        )
+
+
     }
 
 
@@ -29,5 +37,23 @@ function onFormSubmit(event) {
 }
 
 
+function createTableRow(linkName, linkValue) {
+    const trElem = document.createElement("tr");
+
+    const tdName = document.createElement("td");
+    tdName.innerText = linkName;
+
+    trElem.appendChild(
+        tdName
+    );
+
+    const tdValue = document.createElement("td");
+    tdValue.innerText = linkValue;
+    trElem.appendChild(
+        tdValue
+    );
+    return trElem;
+
+}
 
 
