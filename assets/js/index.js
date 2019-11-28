505658d6fa761323516aa0ef1613d4c4b2951672
User Name <artur.it2914@gmail.com>
2019-11-28 16:35:07 +0200

    Fix: regular expration

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,33 @@
 'use strict';
 
-const PATTERN = /<a.*?href=(?:(?:"(.*?)")|(?:'(.*?)')).*?>(.*?)<\/a\s*?>/igm;
+const PATTERN_GLOBAL = /<a.*?href\s*?=\s*?"(.*?)".*?>(.*?)<\/a\s*?>/igm;
+const PATTERN_LOCAL = new RegExp(PATTERN_GLOBAL.source, "i");
+
+const formElem = document.getElementsByTagName("form")[0];
+
+const tableBodyElem = document.getElementsByTagName("tbody")[0];
+
+formElem.onsubmit = onFormSubmit;
+
+
+
+
+function onFormSubmit(event) {
+
+    const result = this.elements["userHtml"].value.match(PATTERN_GLOBAL);
+
+    if(Array.isArray(result)){
+
+        const result2 = result[0].match(PATTERN_LOCAL);
+        console.log(result2);
+    }
+
+
+    return false;
+
+
+}
+
+
 
 
